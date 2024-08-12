906768e46760aed9d3323b4ed377e50d69686f84
ProdStalker <prod.stalker@gmail.com>
2024-08-12 11:48:38 +0200

    Initialize project

---
+++ lib/core/services/router.dart
@@ -0,0 +1,32 @@
+import 'package:education/core/commons/views/page_under_construction.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Route<dynamic> generateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    /*case OnBoardingScreen.routeName:
+      return _pageBuilder(
+        (_) => const OnBoardingScreen(),
+        settings: settings,
+      );*/
+    default:
+      return _pageBuilder(
+        (p0) => const PageUnderConstruction(),
+        settings: settings,
+      );
+  }
+}
+
+PageRouteBuilder<dynamic> _pageBuilder(
+  Widget Function(BuildContext) page, {
+  required RouteSettings settings,
+}) {
+  return PageRouteBuilder(
+    settings: settings,
+    transitionsBuilder: (_, animation, __, child) => FadeTransition(
+      opacity: animation,
+      child: child,
+    ),
+    pageBuilder: (context, _, __) => page(context),
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:education/core/res/colours.dart';
+import 'package:education/core/res/fonts.dart';
+import 'package:education/core/services/router.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Education',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(color: Colors.transparent),
+        colorScheme: ColorScheme.fromSwatch(accentColor: Colours.primaryColour),
+        fontFamily: Fonts.poppins,
+        useMaterial3: true,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      onGenerateRoute: generateRoute,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({required this.title, super.key});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
