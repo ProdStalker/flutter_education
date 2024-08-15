53954c512260bcfef34264e7c5a8a906b07946a2
ProdStalker <prod.stalker@gmail.com>
2024-08-15 11:22:34 +0200

    Sign in

--- lib/src/auth/presentation/views/sign_in_screen.dart
+++ lib/src/auth/presentation/views/sign_in_screen.dart
@@ -1,5 +1,17 @@
-import 'package:education/core/extensions/context_extension.dart';
+import 'package:education/core/commons/app/providers/user_provider.dart';
+import 'package:education/core/commons/widgets/gradient_background.dart';
+import 'package:education/core/commons/widgets/rounded_button.dart';
+import 'package:education/core/res/fonts.dart';
+import 'package:education/core/res/media_res.dart';
+import 'package:education/core/utils/core_utils.dart';
+import 'package:education/src/auth/data/models/user_model.dart';
+import 'package:education/src/auth/presentation/bloc/auth_bloc.dart';
+import 'package:education/src/auth/presentation/views/sign_up_screen.dart';
+import 'package:education/src/auth/presentation/widgets/sign_in_form.dart';
+import 'package:education/src/dashboard/presentation/views/dashboard.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
@@ -11,15 +23,119 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Placeholder(
-      child: Center(
-        child: Text(
-          'SIGN IN SCREEN',
-          style:
-              context.theme.textTheme.bodyLarge?.copyWith(color: Colors.white),
-        ),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (_, state) {
+          if (state is AuthError) {
+            CoreUtils.showSnackBar(context, state.message);
+          } else if (state is SignedIn) {
+            context.read<UserProvider>().initUser(state.user as LocalUserModel);
+            Navigator.pushReplacementNamed(context, Dashboard.routeName);
+          }
+        },
+        builder: (BuildContext context, AuthState state) {
+          return GradientBackground(
+            image: MediaRes.authGradientBackground,
+            child: Center(
+              child: ListView(
+                shrinkWrap: true,
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                children: [
+                  const Text(
+                    'Easy to learn, discover more skills.',
+                    style: TextStyle(
+                      fontFamily: Fonts.aoenik,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 32,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        'Sign in to your account',
+                        style: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                      Baseline(
+                        baseline: 100,
+                        baselineType: TextBaseline.alphabetic,
+                        child: TextButton(
+                          onPressed: () {
+                            Navigator.pushReplacementNamed(
+                              context,
+                              SignUpScreen.routeName,
+                            );
+                          },
+                          child: const Text(
+                            'Register account?',
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  SignInForm(
+                    emailController: emailController,
+                    passwordController: passwordController,
+                    formKey: formKey,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {
+                        // Navigator.pushNamed(context, '/forgot-password');
+                      },
+                      child: const Text('Forgot password'),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  if (state is AuthLoading)
+                    const Center(child: CircularProgressIndicator())
+                  else
+                    RoundedButton(
+                      label: 'Sign In',
+                      onPressed: () {
+                        FocusManager.instance.primaryFocus?.unfocus();
+                        FirebaseAuth.instance.currentUser?.reload();
+                        if (formKey.currentState!.validate()) {
+                          context.read<AuthBloc>().add(
+                                SignInEvent(
+                                  email: emailController.text.trim(),
+                                  password: passwordController.text.trim(),
+                                ),
+                              );
+                        }
+                      },
+                    ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
