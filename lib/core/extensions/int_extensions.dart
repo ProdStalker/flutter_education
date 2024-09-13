1f74c04041ae5ca53375101ef6bedf4f11d21daf
ProdStalker <prod.stalker@gmail.com>
2024-09-13 14:19:29 +0200

    wip(course material ui): exam detail view

--- lib/core/extensions/int_extensions.dart
+++ lib/core/extensions/int_extensions.dart
@@ -17,6 +17,20 @@ extension IntExt on int {
 
   String get displayDuration {
     if (this <= 60) {
+      return '${this}s';
+    }
+    if (this <= 3600) {
+      return '${(this / 60).round()}m';
+    }
+    if (this <= 86400) {
+      return '${(this / 3600).round()}h';
+    }
+
+    return '${(this / 86400).round()}d';
+  }
+
+  String get displayDurationLong {
+    if (this <= 60) {
       return '$this seconds';
     }
     if (this <= 3600) {
--- lib/src/course/features/exams/presentation/views/exam_details_view.dart
+++ lib/src/course/features/exams/presentation/views/exam_details_view.dart
@@ -1,5 +1,16 @@
+import 'package:education/core/commons/widgets/course_info_tile.dart';
+import 'package:education/core/commons/widgets/gradient_background.dart';
+import 'package:education/core/commons/widgets/rounded_button.dart';
+import 'package:education/core/extensions/context_extension.dart';
+import 'package:education/core/extensions/int_extensions.dart';
+import 'package:education/core/res/colours.dart';
+import 'package:education/core/res/media_res.dart';
+import 'package:education/core/utils/core_utils.dart';
+import 'package:education/src/course/features/exams/data/models/exam_model.dart';
 import 'package:education/src/course/features/exams/domain/entities/exam.dart';
+import 'package:education/src/course/features/exams/presentation/cubit/exam_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ExamDetailsView extends StatefulWidget {
   const ExamDetailsView(this.exam, {super.key});
@@ -13,8 +24,146 @@ class ExamDetailsView extends StatefulWidget {
 }
 
 class _ExamDetailsViewState extends State<ExamDetailsView> {
+  late Exam completeExam;
+
+  void getQuestions() {
+    context.read<ExamCubit>().getExamQuestions(widget.exam);
+  }
+
+  @override
+  void initState() {
+    completeExam = widget.exam;
+    super.initState();
+    getQuestions();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: Text(
+          widget.exam.title,
+        ),
+      ),
+      body: GradientBackground(
+        image: MediaRes.documentsGradientBackground,
+        child: BlocConsumer<ExamCubit, ExamState>(
+          listener: (_, state) {
+            if (state is ExamError) {
+              CoreUtils.showSnackBar(context, state.message);
+            } else if (state is ExamQuestionsLoaded) {
+              completeExam = (completeExam as ExamModel).copyWith(
+                questions: state.questions,
+              );
+            }
+          },
+          builder: (context, state) {
+            return SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: ListView(
+                        children: [
+                          Center(
+                            child: Container(
+                              width: 80,
+                              height: 80,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Colours.physicsTileColour,
+                              ),
+                              child: completeExam.imageUrl != null
+                                  ? Image.network(
+                                      completeExam.imageUrl!,
+                                      width: 60,
+                                      height: 60,
+                                      fit: BoxFit.cover,
+                                    )
+                                  : Image.asset(
+                                      MediaRes.test,
+                                      width: 60,
+                                      height: 60,
+                                      fit: BoxFit.cover,
+                                    ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            completeExam.title,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            completeExam.description,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w600,
+                              color: Colours.neutralTextColour,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          CourseInfoTile(
+                            image: MediaRes.examTime,
+                            title: '${completeExam.timeLimit.displayDuration} '
+                                'for the test.',
+                            subtitle: 'Complete the test in '
+                                '${completeExam.timeLimit.displayDurationLong}',
+                          ),
+                          if (state is ExamQuestionsLoaded) ...[
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            CourseInfoTile(
+                              image: MediaRes.examQuestions,
+                              title:
+                                  '${completeExam.questions?.length} questions',
+                              subtitle: 'This test consists of '
+                                  '${completeExam.questions?.length} questions',
+                            ),
+                          ],
+                        ],
+                      ),
+                    ),
+                    if (state is GettingExamQuestions)
+                      const Center(
+                        child: LinearProgressIndicator(),
+                      )
+                    else if (state is ExamQuestionsLoaded)
+                      RoundedButton(
+                        label: 'Start exam',
+                        onPressed: () {
+                          /*Navigator.pushNamed(
+                            context,
+                            ExamView.routeName,
+                            arguments: completeExam,
+                          );*/
+                        },
+                      )
+                    else
+                      Text(
+                        'No questions for this exams',
+                        style: context.theme.textTheme.titleLarge,
+                      ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
